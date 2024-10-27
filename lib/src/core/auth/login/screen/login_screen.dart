be0735cf30c62a894e6ff14ae261be09323c016c
ounchanraksmey <ounchanraksmey29@gmail.com>
2024-10-27 22:30:13 +0700

    unfocus text field when click anywhere

--- lib/src/core/auth/login/screen/login_screen.dart
+++ lib/src/core/auth/login/screen/login_screen.dart
@@ -33,129 +33,138 @@ class _LoginScreenState extends State<LoginScreen> {
             backgroundColor: AppColor.primaryColor,
             body: SafeArea(
               child: Obx(
-                () => SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 80),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          AppLogo.appLogo,
-                          height: 150,
-                          width: 110,
+                () => GestureDetector(
+                  onTap: () {
+                    _usernameFocusNode.unfocus();
+                    _passwordFocusNode.unfocus();
+                  },
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 80),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            AppLogo.appLogo,
+                            height: 150,
+                            width: 110,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 64),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: AppStyle.horizontalPadding,
-                        ),
-                        child: CustomTextField(
-                          focusNode: _usernameFocusNode,
-                          errorDescription:
-                              _loginController.invalidUsernameDescription.value,
-                          isShowError: _loginController.isInvalidUsername.value,
-                          controller: _loginController
-                              .usernameTextEditingController.value,
-                          onChangeTextField: (value) {
-                            _loginController.resetUsernameError();
-                          },
-                          label: "Username",
-                        ),
-                      ),
-                      const SizedBox(height: 24),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: AppStyle.horizontalPadding,
-                        ),
-                        child: CustomTextField(
-                          focusNode: _passwordFocusNode,
-                          errorDescription:
-                              _loginController.invalidPasswordDescription.value,
-                          isShowError: _loginController.isInvalidPassword.value,
-                          controller: _loginController
-                              .passwordTextEditingController.value,
-                          onChangeTextField: (value) {
-                            _loginController.resetPasswordError();
-                          },
-                          obscureText: _loginController.isHidePassword.value,
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              _loginController.changePasswordVisibilityState();
+                        const SizedBox(height: 64),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppStyle.horizontalPadding,
+                          ),
+                          child: CustomTextField(
+                            focusNode: _usernameFocusNode,
+                            errorDescription: _loginController
+                                .invalidUsernameDescription.value,
+                            isShowError:
+                                _loginController.isInvalidUsername.value,
+                            controller: _loginController
+                                .usernameTextEditingController.value,
+                            onChangeTextField: (value) {
+                              _loginController.resetUsernameError();
                             },
-                            child: Icon(
-                              _loginController.isHidePassword.value == true
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                            ),
-                          ),
-                          label: "Password",
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: GestureDetector(
-                          onTap: () {
-                            debugPrint("Forgot password?");
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 20),
-                            child: Text(
-                              "Forgot Password?",
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
+                            label: "Username",
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 24),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: AppStyle.horizontalPadding,
-                        ),
-                        child: FilledButton(
-                          onPressed: () async {
-                            _usernameFocusNode.unfocus();
-                            _passwordFocusNode.unfocus();
-                            await _loginController.loginUser(
-                              onSuccess: () {
-                                context.go(
-                                  AppScreen.dashboardScreen.path,
-                                  extra: UserType.loggedInUser,
-                                );
+                        const SizedBox(height: 24),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppStyle.horizontalPadding,
+                          ),
+                          child: CustomTextField(
+                            focusNode: _passwordFocusNode,
+                            errorDescription: _loginController
+                                .invalidPasswordDescription.value,
+                            isShowError:
+                                _loginController.isInvalidPassword.value,
+                            controller: _loginController
+                                .passwordTextEditingController.value,
+                            onChangeTextField: (value) {
+                              _loginController.resetPasswordError();
+                            },
+                            obscureText: _loginController.isHidePassword.value,
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                _loginController
+                                    .changePasswordVisibilityState();
                               },
-                            );
-                          },
-                          child: Text(
-                            "Login",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge
-                                ?.copyWith(fontWeight: FontWeight.bold),
+                              child: Icon(
+                                _loginController.isHidePassword.value == true
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
+                            label: "Password",
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 24),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: AppStyle.horizontalPadding,
-                        ),
-                        child: FilledButton(
-                          onPressed: () {
-                            context.pop();
-                          },
-                          child: Text(
-                            "Back",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge
-                                ?.copyWith(fontWeight: FontWeight.bold),
+                        const SizedBox(height: 12),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: GestureDetector(
+                            onTap: () {
+                              debugPrint("Forgot password?");
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 20),
+                              child: Text(
+                                "Forgot Password?",
+                                style: Theme.of(context).textTheme.bodySmall,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 24),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppStyle.horizontalPadding,
+                          ),
+                          child: FilledButton(
+                            onPressed: () async {
+                              _usernameFocusNode.unfocus();
+                              _passwordFocusNode.unfocus();
+                              await _loginController.loginUser(
+                                onSuccess: () {
+                                  context.go(
+                                    AppScreen.dashboardScreen.path,
+                                    extra: UserType.loggedInUser,
+                                  );
+                                },
+                              );
+                            },
+                            child: Text(
+                              "Login",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppStyle.horizontalPadding,
+                          ),
+                          child: FilledButton(
+                            onPressed: () {
+                              context.pop();
+                            },
+                            child: Text(
+                              "Back",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
